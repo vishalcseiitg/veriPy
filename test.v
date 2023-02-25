88fd61596b78349d615f0efc622b0ea8b62239b8
Vishal Kumar <vishalsharma.viz@gmail.com>
2023-02-25 16:56:04 +0530

    Add files via upload

---
+++ test.v
@@ -0,0 +1,5 @@
+module full_adder(input a, input b, input carry_in, output sum, output carry_out);
+    assign sum = a ^ b ^ carry_in;
+    assign carry_out = (a & b) | (carry_in & (a ^ b));
+endmodule
+
